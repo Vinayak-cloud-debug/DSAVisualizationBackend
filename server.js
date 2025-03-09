4b0e2038ce5ac9764fbfc35b262ee98dbb6b4c2e
Vinayak-cloud-debug <vinayakrodd56@gmail.com>
2025-03-09 20:30:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
-const authRoutes = require("./routes/auth.routes.js");
-const connectToMongoDB = require("./db/connectToMongoDB");
+const authRoutes = require("./auth.routes.js");
+const connectToMongoDB = require("./connectToMongoDB");
 const cors = require('cors');
+
+const User = require("./user.models.js"); // Your Mongoose User model
 dotenv.config();
 
 const app = express();
 
+const allowedOrigins = ['https://algorithmerz.in', 'https://dsavisualizationbackend-tmii.onrender.com','https://dsavisualizationfrontend-eccu.onrender.com'];
+
+
 app.use(cors({
-	origin: 'http://localhost:5173', // Allow requests from this frontend
-	methods: 'GET,POST,PUT,DELETE',
-	allowedHeaders: 'Content-Type,Authorization',
-	credentials:true
-  }));
-  
+  origin: allowedOrigins, // Allow specific origins
+  methods: ['GET', 'POST'], // Allow specific HTTP methods (adjust as needed)
+  credentials: true, // If you need to send cookies or authorization headers
+}));
 
 
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
@@ -24,7 +27,21 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
+app.use("/api/auth",authRoutes);
+
+
+
+
+app.get("/api/user-count", async (req, res) => {
+  try {
+    const count = await User.countDocuments();
+    console.log(count);
+    res.json({count});
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user count", error });
+  }
+});
+
 
 
 
@@ -32,3 +49,22 @@ app.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 });
+
+
+
+
+const BACKEND_URL = "https://dsavisualizationbackend-4.onrender.com"; // Replace with your real backend URL
+
+setInterval(async () => {
+    try {
+        await fetch(`${BACKEND_URL}/api/auth/login`);
+        console.log("Pinged server to keep it awake");
+    } catch (err) {
+        console.error("Ping failed", err);
+    }
+}, 60 * 1000); // Ping every 1 minute
+
+
+
+
+
